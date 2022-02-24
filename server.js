c7e71f31abb991e0f5fb91d3f6f4e341680ee303
ardalangh <rdghoorchian@gmail.com>
2022-02-24 12:17:10 -0800

    debugged the raycaster issue

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const path = require('path');
-const express = require('express');
+var path = require('path');
+var express = require('express');
 
-const app = express();
+var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
-
 app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
